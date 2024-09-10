c0f33984e3acd7f96c3ba5127a88f88044c0d3e7
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2024-09-10 12:27:55 +0530

    feat: Badge Component added

--- codes-react-bootstrap/src/App.jsx
+++ codes-react-bootstrap/src/App.jsx
@@ -1,12 +1,14 @@
 // import "./App.css";
 
 import AlertExample from "./Components/AlertExample";
+import BadgeExample from "./Components/BadgeExample";
 
 function App() {
   return (
     <>
       Hello World!
       <AlertExample />
+      <BadgeExample />
     </>
   );
 }
